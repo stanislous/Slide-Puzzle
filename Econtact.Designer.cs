8fcb1fcebcf33fd788424c035011bf086d88faf9
stanislous <stanislousvanderputt@gmail.com>
2018-08-11 01:20:50 +0530

    Apply Update, Delete and Add Features

---
+++ Econtact.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace FirstWindowsFormApp
+{
+    partial class Econtact
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Econtact));
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.contactId = new System.Windows.Forms.Label();
+            this.textContactId = new System.Windows.Forms.TextBox();
+            this.textFirstName = new System.Windows.Forms.TextBox();
+            this.firstName = new System.Windows.Forms.Label();
+            this.textLastName = new System.Windows.Forms.TextBox();
+            this.lastName = new System.Windows.Forms.Label();
+            this.textContactNo = new System.Windows.Forms.TextBox();
+            this.contactNo = new System.Windows.Forms.Label();
+            this.textAddress = new System.Windows.Forms.TextBox();
+            this.address = new System.Windows.Forms.Label();
+            this.textGender = new System.Windows.Forms.TextBox();
+            this.gender = new System.Windows.Forms.Label();
+            this.add = new System.Windows.Forms.Button();
+            this.update = new System.Windows.Forms.Button();
+            this.delete = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.search = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(358, 23);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(127, 49);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // contactId
+            // 
+            this.contactId.AutoSize = true;
+            this.contactId.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.contactId.Location = new System.Drawing.Point(41, 96);
+            this.contactId.Name = "contactId";
+            this.contactId.Size = new System.Drawing.Size(78, 18);
+            this.contactId.TabIndex = 2;
+            this.contactId.Text = "Contact ID";
+            this.contactId.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textContactId
+            // 
+            this.textContactId.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textContactId.Location = new System.Drawing.Point(134, 96);
+            this.textContactId.Name = "textContactId";
+            this.textContactId.Size = new System.Drawing.Size(149, 26);
+            this.textContactId.TabIndex = 3;
+            // 
+            // textFirstName
+            // 
+            this.textFirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textFirstName.Location = new System.Drawing.Point(134, 139);
+            this.textFirstName.Name = "textFirstName";
+            this.textFirstName.Size = new System.Drawing.Size(149, 26);
+            this.textFirstName.TabIndex = 5;
+            this.textFirstName.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // firstName
+            // 
+            this.firstName.AutoSize = true;
+            this.firstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firstName.Location = new System.Drawing.Point(41, 139);
+            this.firstName.Name = "firstName";
+            this.firstName.Size = new System.Drawing.Size(81, 18);
+            this.firstName.TabIndex = 4;
+            this.firstName.Text = "First Name";
+            // 
+            // textLastName
+            // 
+            this.textLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textLastName.Location = new System.Drawing.Point(134, 187);
+            this.textLastName.Name = "textLastName";
+            this.textLastName.Size = new System.Drawing.Size(149, 26);
+            this.textLastName.TabIndex = 7;
+            // 
+            // lastName
+            // 
+            this.lastName.AutoSize = true;
+            this.lastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lastName.Location = new System.Drawing.Point(41, 187);
+            this.lastName.Name = "lastName";
+            this.lastName.Size = new System.Drawing.Size(80, 18);
+            this.lastName.TabIndex = 6;
+            this.lastName.Text = "Last Name";
+            this.lastName.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textContactNo
+            // 
+            this.textContactNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textContactNo.Location = new System.Drawing.Point(134, 238);
+            this.textContactNo.Name = "textContactNo";
+            this.textContactNo.Size = new System.Drawing.Size(149, 26);
+            this.textContactNo.TabIndex = 9;
+            // 
+            // contactNo
+            // 
+            this.contactNo.AutoSize = true;
+            this.contactNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.contactNo.Location = new System.Drawing.Point(41, 238);
+            this.contactNo.Name = "contactNo";
+            this.contactNo.Size = new System.Drawing.Size(84, 18);
+            this.contactNo.TabIndex = 8;
+            this.contactNo.Text = "Contact No";
+            // 
+            // textAddress
+            // 
+            this.textAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textAddress.Location = new System.Drawing.Point(134, 283);
+            this.textAddress.Multiline = true;
+            this.textAddress.Name = "textAddress";
+            this.textAddress.Size = new System.Drawing.Size(149, 88);
+            this.textAddress.TabIndex = 11;
+            // 
+            // address
+            // 
+            this.address.AutoSize = true;
+            this.address.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.address.Location = new System.Drawing.Point(41, 283);
+            this.address.Name = "address";
+            this.address.Size = new System.Drawing.Size(62, 18);
+            this.address.TabIndex = 10;
+            this.address.Text = "Address";
+            // 
+            // textGender
+            // 
+            this.textGender.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textGender.Location = new System.Drawing.Point(134, 388);
+            this.textGender.Name = "textGender";
+            this.textGender.Size = new System.Drawing.Size(149, 26);
+            this.textGender.TabIndex = 13;
+            this.textGender.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // gender
+            // 
+            this.gender.AutoSize = true;
+            this.gender.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gender.Location = new System.Drawing.Point(41, 388);
+            this.gender.Name = "gender";
+            this.gender.Size = new System.Drawing.Size(57, 18);
+            this.gender.TabIndex = 12;
+            this.gender.Text = "Gender";
+            this.gender.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // add
+            // 
+            this.add.BackColor = System.Drawing.Color.SpringGreen;
+            this.add.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.add.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.add.Location = new System.Drawing.Point(17, 430);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(86, 36);
+            this.add.TabIndex = 14;
+            this.add.Text = "Add";
+            this.add.UseVisualStyleBackColor = false;
+            // 
+            // update
+            // 
+            this.update.BackColor = System.Drawing.SystemColors.Highlight;
+            this.update.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.update.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.update.Location = new System.Drawing.Point(134, 430);
+            this.update.Name = "update";
+            this.update.Size = new System.Drawing.Size(86, 36);
+            this.update.TabIndex = 15;
+            this.update.Text = "Update";
+            this.update.UseVisualStyleBackColor = false;
+            // 
+            // delete
+            // 
+            this.delete.BackColor = System.Drawing.Color.DarkRed;
+            this.delete.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.delete.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.delete.Location = new System.Drawing.Point(259, 430);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(86, 36);
+            this.delete.TabIndex = 16;
+            this.delete.Text = "Delete";
+            this.delete.UseVisualStyleBackColor = false;
+            // 
+            // clear
+            // 
+            this.clear.BackColor = System.Drawing.Color.Gold;
+            this.clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clear.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.clear.Location = new System.Drawing.Point(374, 430);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(86, 36);
+            this.clear.TabIndex = 17;
+            this.clear.Text = "Clear";
+            this.clear.UseVisualStyleBackColor = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(322, 139);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(528, 275);
+            this.dataGridView1.TabIndex = 18;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(322, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 20);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Search";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // search
+            // 
+            this.search.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.search.Location = new System.Drawing.Point(389, 95);
+            this.search.Name = "search";
+            this.search.Size = new System.Drawing.Size(461, 26);
+            this.search.TabIndex = 20;
+            this.search.TextChanged += new System.EventHandler(this.search_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(817, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(33, 30);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 21;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Econtact
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(862, 479);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.search);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.update);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.textGender);
+            this.Controls.Add(this.gender);
+            this.Controls.Add(this.textAddress);
+            this.Controls.Add(this.address);
+            this.Controls.Add(this.textContactNo);
+            this.Controls.Add(this.contactNo);
+            this.Controls.Add(this.textLastName);
+            this.Controls.Add(this.lastName);
+            this.Controls.Add(this.textFirstName);
+            this.Controls.Add(this.firstName);
+            this.Controls.Add(this.textContactId);
+            this.Controls.Add(this.contactId);
+            this.Controls.Add(this.pictureBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Econtact";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Econtact_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label contactId;
+        private System.Windows.Forms.TextBox textContactId;
+        private System.Windows.Forms.TextBox textFirstName;
+        private System.Windows.Forms.Label firstName;
+        private System.Windows.Forms.TextBox textLastName;
+        private System.Windows.Forms.Label lastName;
+        private System.Windows.Forms.TextBox textContactNo;
+        private System.Windows.Forms.Label contactNo;
+        private System.Windows.Forms.TextBox textAddress;
+        private System.Windows.Forms.Label address;
+        private System.Windows.Forms.TextBox textGender;
+        private System.Windows.Forms.Label gender;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button update;
+        private System.Windows.Forms.Button delete;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox search;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ econtactClasses/contactClass.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstWindowsFormApp.econtactClasses
+{
+    class contactClass
+    {
+        //Getter Setter Properties
+        //Acts as Data Carrier in Our Application
+        public int ContactID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ContactNo { get; set; }
+        public string Address { get; set; }
+        public string Gender { get; set; }
+
+        static string myconnstrng = ConfigurationManager.ConnectionStrings["connstrng"].ConnectionString;
+
+        //Selecting Data from Database
+        public DataTable Select()
+        {
+            ///Step 1: Database Connection
+            SqlConnection conn = new SqlConnection(myconnstrng);
+            DataTable dt = new DataTable();
+            try
+            {
+                //Step 2: Writing SQL Query
+                string sql = "SELECT * FROM tbl_contact";
+                //Creating cmd using sql and conn
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                //Creating SQL DataAdapter using cmd
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                conn.Open();
+                adapter.Fill(dt);
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return dt;
+        }
+        //Inserting Data into Database
+        public bool Insert(contactClass c)
+        {
+            //Creating a default return type and setting its value to false
+            bool isSuccess = false;
+
+            //Step 1: Connect Database
+            SqlConnection conn = new SqlConnection(myconnstrng);
+            try
+            {
+                //Step 2: Create a SQL Qury to insert Data
+                string sql = "INSERT INTO tbl_contact (FirstName, LastName, ContactNo, Address, Gender) VALUES (@FirstName, @LastName, @ContactNo, @Address, @Gender)";
+                //Creating cmd using sql and conn
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                //Create Parameters to add data
+                cmd.Parameters.AddWithValue("@FirstName", c.FirstName);
+                cmd.Parameters.AddWithValue("@LastName", c.LastName);
+                cmd.Parameters.AddWithValue("@ContactNo", c.ContactNo);
+                cmd.Parameters.AddWithValue("@Address", c.Address);
+                cmd.Parameters.AddWithValue("@Gender", c.Gender);
+
+                //Connection Open Here
+                conn.Open();
+                int rows = cmd.ExecuteNonQuery();
+                //If the Query runs successfully then the value of rows will 
+                //be greater than zero else its value will be 0
+                if (rows > 0)
+                {
+                    isSuccess = true;
+                }
+                else
+                {
+                    isSuccess = false;
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return isSuccess;
+        }
+
+        //Method to update fata in database from our application
+        public bool Update(contactClass c)
+        {
+            //Create a default return type and set its default value to false
+            bool isSuccess = false;
+            SqlConnection conn = new SqlConnection(myconnstrng);
+            try
+            {
+                //SQL to upate data in our Database
+                string sql = "UPDATE tbl_contact SET FirstName=@FirstName, LastName=@LastName, ContactNo=@ContactNo, Address=@Address, Gender=@Gender";
+
+                //Creating SQL Command
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                //Creating Parameters to add value
+                cmd.Parameters.AddWithValue("@FirstName", c.FirstName);
+                cmd.Parameters.AddWithValue("@LastName", c.LastName);
+                cmd.Parameters.AddWithValue("@ContactNo", c.ContactNo);
+                cmd.Parameters.AddWithValue("@Address", c.Address);
+                cmd.Parameters.AddWithValue("@Gender", c.Gender);
+                cmd.Parameters.AddWithValue("@ContactID", c.ContactID);
+                //Open Database Connection
+                conn.Open();
+
+                int rows = cmd.ExecuteNonQuery();
+                if (rows > 0)
+                {
+                    isSuccess = true;
+                }
+                else
+                {
+                    isSuccess = false;
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return isSuccess;
+        }
+        //Metod to Delete Data from Database
+        public bool Delete(contactClass c)
+        {
+            //Create a default return value and set its value to false
+            bool isSuccess = false;
+            //Create SQL Connection
+            SqlConnection conn = new SqlConnection(myconnstrng);
+            try
+            {
+                //SQL to Delete Data
+                string sql = "DELETE FROM tbl_contact WHERE ContactID=@ContactID";
+
+                //Creating SQL Command
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@ContactID", c.ContactID);
+                //Open Connexction
+                conn.Open();
+                int rows = cmd.ExecuteNonQuery();
+                //If the query run sucessfully then the value of is greter than zero else its value is 0
+                if(rows > 0)
+                {
+                    isSuccess = true;
+                }
+                else
+                {
+                    isSuccess = false;
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return isSuccess;
+        }
+    }
+}
